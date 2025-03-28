493f5d41a9f146d2907b06e98653d90b10fa6788
litch <matheushyago92@gmail.com>
2025-03-28 02:34:37 -0300

    ajuste

--- Backend/src/main/java/com/testelemontech/solicitacoes/repository/ModelRequestRepository.java
+++ Backend/src/main/java/com/testelemontech/solicitacoes/repository/ModelRequestRepository.java
@@ -3,9 +3,8 @@ package com.testelemontech.solicitacoes.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.testelemontech.solicitacoes.model.ModelRequest;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface ModelRequestRepository extends JpaRepository<ModelRequest, Long> {
 
 }
